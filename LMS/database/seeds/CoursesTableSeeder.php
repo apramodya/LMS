09b224c57e4ad087d97aca89c5ab323970692385
apramodya <pramodyaabeysinghe@gmail.com>
2018-03-12 12:08:22 +0530

    Seed courses
    add dummy courses data

---
+++ LMS/database/seeds/CoursesTableSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CoursesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $course = new \App\Course([
+            'course_id' => 'SCS1001',
+            'name' => 'Programming I',
+            'enrollment_key' => 'SCS1001',
+            'year' => '1',
+            'degree' => 'CS',
+        ]);
+        $course->save();
+
+        $course = new \App\Course([
+            'course_id' => 'SCS1002',
+            'name' => 'Programming II',
+            'enrollment_key' => 'SCS1002',
+            'year' => '1',
+            'degree' => 'CS',
+        ]);
+        $course->save();
+
+        $course = new \App\Course([
+            'course_id' => 'SCS2003',
+            'name' => 'Programming III',
+            'enrollment_key' => 'SCS2001',
+            'year' => '2',
+            'degree' => 'CS',
+        ]);
+        $course->save();
+
+        $course = new \App\Course([
+            'course_id' => 'SCS2004',
+            'name' => 'Programming IV',
+            'enrollment_key' => 'SCS2004',
+            'year' => '2',
+            'degree' => 'CS',
+        ]);
+        $course->save();
+
+    }
+}
--- LMS/database/seeds/DatabaseSeeder.php
+++ LMS/database/seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserTableSeeder::class);
         $this->call(PositionsTableSeeder::class);
+        $this->call(CoursesTableSeeder::class);
+
     }
 }
