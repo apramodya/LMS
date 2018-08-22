601c7b897b732b674d2a98a99f230fdbe7a204c8
udeshsendanayake <usendanayake@gmail.com>
2018-08-23 00:28:03 +0530

    UI- Student Gpa view modified

--- LMS/resources/views/student/gpa.blade.php
+++ LMS/resources/views/student/gpa.blade.php
@@ -5,55 +5,71 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-6 offset-3">
-                <div class="card">
+            <div class="col-md-6">
+            <div style ="text-align:center;"class="card">
                     <div class="card-header">
                         <span> <strong>Overview of {{$student->first_name}} {{$student->last_name}}</strong> </span>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body" style ="text-align:center !important;background-color:#746d9a2e;">
                         <div class="row">
-                            {{--gpa--}}
-                            <div class="col-md-8"> <h4>Current GPA: {{$gpaFinal}}</h4></div>
-                        </div>
-                        <div class="row">
-                            {{--rank--}}
-                            <div class="col-md-3">#16</div>
-                            {{--creadits--}}
-                            <div class="col-md-3">60</div>
-                        </div>
+                            {{--gpa--}}  
+                            <div class="col-md-12"> <span style ="font-size: 40px;">{{$gpaFinal}}</span><br><h4>Current GPA  </h4></div>                           
+                        </div>                            
                     </div>
-
-                    {{--@foreach($age as $x=>$x_value)--}}
-
-                        {{--{{$x}} {{$x_value}}--}}
-
-{{--{{$rank}}--}}
-                    {{--@endforeach--}}
-
                 </div>
             </div>
+            <div class="col-md-6">
+
+                    <div style ="text-align:center;" class="card">
+                        <div class="card-header">
+                                <span> <strong>Degree Infromation</strong> </span>
+                        </div>
+                        <div class="card-body" style ="text-align:center !important;background-color:#746d9a2e;">
+                            <div class="row">
+                                {{--gpa--}}
+                                
+                                <div class="col-md-12"> <span style ="font-size: 40px;">{{$gpaFinal}}</span><br><h4>Current GPA  </h4></div>                               
+                            </div>                               
+                        </div>
+                    </div
+            </div>
         </div>
+</div>
+
+
+
+
+
 <br>
-
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
         <div class="card">
-            <div class="card-header">
-                <span><strong>Student Ranking</strong></span>
-            </div>
-            <div class="card-body">
-                <div class="card">
-                    <ul class="list-group list-group-flush">
-                        <?php $count = 0; ?>
-                                    @foreach($sortedRanks as $key => $sortedRank)
-                             <?php   $count = $count + 1 ?>
-                                    <li class="list-group-item"><strong>#{{$count}} {{$key}}</strong>
-                                        {{--<span class="small">John Doe</span>--}}
-                                        <span class="float-right font-weight-bold"><strong>{{$sortedRank}}</strong></span>
-                                    </li>
-                                    @endforeach
-                    </ul>
+                <div class="card-header">
+                    <span><strong>Student Ranking</strong></span>
+                </div>
+                <div class="card-body" style ="background-color: #746d9a2e;">
+                    <div class="card">
+                        <ul class="list-group list-group-flush">
+                            <?php $count = 0; ?>
+                                        @foreach($sortedRanks as $key => $sortedRank)
+                                <?php   $count = $count + 1 ?>
+                                        <li class="list-group-item"><strong>#{{$count}} {{$key}}</strong>
+                                            {{--<span class="small">John Doe</span>--}}
+                                            <span class="float-right font-weight-bold"><strong>{{$sortedRank}}</strong></span>
+                                        </li>
+                                        @endforeach
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
+        </div>
+</div>
+
+
+
+
 
 
 
